519bba3b0ba9f226de1d9c750924a2841b8601a3
HyungwonJin <86827200+HyungwonJin@users.noreply.github.com>
2021-08-07 23:57:40 +0900

    feat: User model 수정 & bcrypt 설치

--- src/models/User.js
+++ src/models/User.js
@@ -1,11 +1,16 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = mongoose.Schema({
-    email: String,
-    username: String,
-    password: String,
-    language: String
+    email: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    language: { type: String }
 })
+
+userSchema.pre("save", async function () {
+    this.password = await bcrypt.hash(this.password, 5);
+}) // password를 저장하기 전에 가로채서 암호화함
 
 const User = mongoose.model("User", userSchema);
 
