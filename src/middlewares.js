606fea09e6a63958bda8869a7a8fce1564e8f963
HyungwonJin <86827200+HyungwonJin@users.noreply.github.com>
2021-08-12 23:44:25 +0900

    feat: Add protectorMiddleware

--- src/middlewares.js
+++ src/middlewares.js
@@ -3,3 +3,11 @@ export const localsMiddleware = (req, res, next) => {
     res.locals.loggedInUser = req.session.user;
     next();
 }
+
+export const protectorMiddleware = (req, res, next) => {
+    if (req.session.loggedIn) {
+        next();
+    } else {
+        return res.redirect("/login");
+    }
+}
--- src/router/wordRouter.js
+++ src/router/wordRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { deleteWord, detail, getEdit, getUpload, postEdit, postUpload } from "../controller/wordController";
+import { protectorMiddleware } from "../middlewares";
 
 const wordRouter = express.Router();
 
 wordRouter.get("/:id([0-9a-f]{24})", detail);
-wordRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
-wordRouter.route("/:id([0-9a-f]{24})/delete").get(deleteWord);
-wordRouter.route("/upload").get(getUpload).post(postUpload);
+wordRouter.route("/:id([0-9a-f]{24})/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
+wordRouter.route("/:id([0-9a-f]{24})/delete").all(protectorMiddleware).get(deleteWord);
+wordRouter.route("/upload").all(protectorMiddleware).get(getUpload).post(postUpload);
 
 export default wordRouter;
