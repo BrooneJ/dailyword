44b8e3ec0e780201fff6a721e023a727e13d9126
HyungwonJin <86827200+HyungwonJin@users.noreply.github.com>
2021-10-07 11:41:09 +0900

    fix: 예외적인 단어가 발견 되어 수정(くすむ)
    くすんだピンク色がお気に入りです에서 3글자 중 뒤에서 두번째 글자 す가 です의 す와 일치해서 문장 전체가 빨간 글씨로 바뀌는 문제 수정

--- src/client/js/detectWord.js
+++ src/client/js/detectWord.js
@@ -4,25 +4,41 @@ const example = document.querySelector('.exampleSet');
 if (title) {
     const titleWord = title.innerText;
     const firstLetter = titleWord[0];
+    const secondLetter = titleWord[1];
     const lastSecondLetter = titleWord[titleWord.length - 2];
     const lastLetter = titleWord[titleWord.length - 1];
     const wordLength = titleWord.length;
     let regexp;
+    let regexpAnother;
     let selectedWord;
     let changeWord;
     for (i = 1; i < example.childNodes[1].childNodes.length; i++) {
         let targetWord = example.childNodes[1].childNodes[i].innerHTML;
         if (wordLength === 1) { // 한 글자 일 때
             regexp = new RegExp(`${firstLetter}`, 'gi');
+            selectedWord = targetWord.match(regexp);
+            changeWord = targetWord.replace(regexp, `<p style="color:red; display:inline-block">${selectedWord}</p>`);
         }
         if (wordLength === 2) { // 두 글자 일 때
-            regexp = new RegExp(`${firstLetter}${lastLetter}?(って)?(いた)?(り)?(った)?(し)?`, 'gi');
+            regexp = new RegExp(`${firstLetter}${lastLetter}?(って)?(いた)?(り)?(った)?(し)?(して)?(んだ)?(んで)?`, 'gi');
+            selectedWord = targetWord.match(regexp);
+            changeWord = targetWord.replace(regexp, `<p style="color:red; display:inline-block">${selectedWord}</p>`);
         }
         if (wordLength >= 3) { // 세 글자 일 때
-            regexp = new RegExp(`${firstLetter}.*${lastSecondLetter}${lastLetter}?(って)?(いた)?(り)?(った)?(し)?`, 'gi');
+            regexp = new RegExp(`${firstLetter}.*${lastSecondLetter}${lastLetter}?(って)?(いた)?(り)?(った)?(し)?(して)?(んだ)?(んで)?`, 'gi');
+            regexpAnother = new RegExp(`${firstLetter}${secondLetter}(って)?(いた)?(り)?(った)?(し)?(して)?(んだ)?(んで)?`, 'gi');
+            const shortMatch = targetWord.match(regexpAnother); // 짧게 일치하는 문장
+            const longMatch = targetWord.match(regexp); // 길게 일치하는 문장
+            if (shortMatch[0].length === 2) { // 만약 첫 두글자만 일치한다면 별도의 처리가 필요하지 않기 때문에
+                shortMatch[0] = 'ustfd96gs9df786s9dfgs9df7g69sdfgt87s9dfvgsdh0'; // random한 긴 문자로 교체
+            }
+            selectedWord = shortMatch[0].length < longMatch[0].length ? shortMatch : longMatch; // 더 짧은 쪽의 단어로 교체
+            if (selectedWord === shortMatch) {
+                changeWord = targetWord.replace(regexpAnother, `<p style="color:red; display:inline-block">${selectedWord}</p>`);
+            } else {
+                changeWord = targetWord.replace(regexp, `<p style="color:red; display:inline-block">${selectedWord}</p>`);
+            }
         }
-        const selectedWord = targetWord.match(regexp);
-        let changeWord = targetWord.replace(regexp, `<p style="color:red; display:inline-block">${selectedWord}</p>`);
         example.childNodes[1].childNodes[i].innerHTML = changeWord;
     }
 }
